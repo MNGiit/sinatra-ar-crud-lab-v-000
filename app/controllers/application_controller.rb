33a4fc9c95355795b552947b0de10fe1947bee36
Mohammed Noori <kmanoorib@gmail.com>
2018-12-29 06:37:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
   
   get "/articles/:id" do
-    @article = Article.find_by_id(:id)
+    @article = Article.find(params[:id])
     erb :articles
   end
   
